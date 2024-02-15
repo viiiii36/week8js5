e010cf27b83aa249ae531181b0e2c56b32aeec75
viiiii36 <vi2015nguyen2015@gmail.com>
2024-02-14 22:34:10 -0800

    almost done

--- week8.js
+++ week8.js
@@ -1,7 +1,7 @@
 /*Use at least one array.
 Use at least two classes.
 Your menu should have the options to create, view, and delete elements.*/
-/*Creating an app that let technicians check-in and check-out tool available in inventory*/
+/*Creating a platform that let user add in and check-out tool available in inventory*/
 //Creating a tool class to store information about the tool in inventory, it can show if the tool can be used or not based 
 //on future calibration date
 class Tool{
@@ -15,33 +15,94 @@ class Tool{
         let timestamp = new Date();
         //check if the tool is valid to use or it needs to be calibrated
         if (this.futureCalibration>timestamp){
-            console.log(`The ${this.type} with ID number ${this.ID} is valid to use`);
+            alert(`The ${this.type} with ID number ${this.ID} is valid to use`);
         }
-        else {console.log(`The ${this.type} with ID number ${this.ID} CANNOT be used. Please schedule for calibration.`)};
+        else {alert(`The ${this.type} with ID number ${this.ID} CANNOT be used. Please schedule for calibration.`)};
     }
 }
-//Showing the current date
-console.log(Date());
-//Option to add new tool item to tool list
-class List{
-    constructor(){
-      this.inventory=[];
+
+//create a list of tasks that operator can do to the tool inventory
+class Task{
+  constructor(){
+    this.list=[];
+  }
+//method to show all available tasks
+start(){
+  let selection = this.showAllTasks();
+  //if user choose anything from 1-4 then there are 4 things they can do on the task list
+  while (selection!=0){
+    switch(selection){
+    case '1':
+      this.submitNewTool();
+      break;
+    case '2':
+      this.showInventory();
+      break;
+    case '3':
+      this.deleteTool();
+      break;
+    case '4':
+      this.checkingOut();
+      break; 
+    default: selection =0;
     }
-    addTool(newTool){
-      this.inventory.push(newTool);
-    }
-    displayTool(){
-      for (let i=0;i<this.inventory.length;i++)
+    //keep popping up the tasks list after user chose the task action
+    selection=this.showAllTasks();
+  }
+//when 0 is chosen then it will close the task list
+  alert('Closing Task List')
+}
+showAllTasks(){
+  return prompt(`
+  0) exit
+  1) Submit new tool into the system
+  2) Show available tools in inventory
+  3) Delete tool
+  4) Check out a tool
+  `);
+}
+//Add a new tool to the list
+submitNewTool(){
+  let type = prompt(`What type of tool?`);
+  let ID = prompt(`Please input ID number:`);
+  let futureCalibration=prompt(`Enter the date for future calibration in format (Month DD, YYYY)`);
+  //this.list.addTool
+  let newTool= new Tool(type,ID,futureCalibration);
+  this.list.push(newTool);
+}
+showInventory(){
+      let listString = '';
+      for (let i=0;i<this.list.length;i++)
       {
-        console.log(`The ${this.inventory[i].type} with ID ${this.inventory[i].ID} needs calibration on ${this.inventory[i].futureCalibration}\n`);
+        listString += i+ ') ' + ' Tool '+ this.list[i].type+' with ID '+ this.list[i].ID + ' needs calibration on '+
+        this.list[i].futureCalibration+ '\n';
       }
-    }
+      alert(listString);
 }
-let tool1=new Tool("pin",45,"Jan 1, 2024");
+deleteTool(){
+  let index = prompt('Enter the index of tool you want to delete ');
+  if (index > -1 && index < this.list.length) {
+  this.list.splice(index,1);
+}
+}
+checkingOut(){
+  let index = prompt('Enter the of tool you want to checkout ');
+  if (index > -1 && index < this.list.length) {
+  this.list[index].describe();
+  if (this.futureCalibration>timestamp){
+    this.teams.splice(index,1);
+  menu.start();
+}
+}
+}
+}
+let menu=new Task()
+menu.start()
+/*let tool1=new Tool("pin",45,"Jan 1, 2024");
 let tool2=new Tool("gage",12,"March 1, 2024");
   tool1.describe();
   tool2.describe();
   let list=new List();
   list.addTool(tool1);
   list.addTool(tool2);
-  list.displayTool();
+  list.displayToolList();*/
